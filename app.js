08258e91315213da12c81f021e1fd377945c25a2
Lyle Nelson <Nelsonlylev@yahoo.com>
2020-12-10 16:14:06 -0700

    Javascript Tests

---
+++ app.js
@@ -0,0 +1,26 @@
+
+let ar1=['This','is','a','split','sentence']
+function rearrange(ar1){
+for(let z=0;z<ar1.length;z++){
+word=ar1[z]
+if (z>0)(console.log(word))}
+console.log(ar1[0])}
+let array1=rearrange(ar1)
+console.log(array1)
+
+let ar2=[6,4,8,33,42,10]
+function largestNum(ar2){
+let largest=0
+for(let x=0; x<ar2.length; x++){
+if(ar2[x]>largest)(largest=ar2[x])}
+return(largest)}
+let array2=largestNum(ar2)
+console.log(array2)
+
+let ar3=[4,2,1,7]
+function elemTimesLength(ar3){
+for(let y=0; y<ar3.length;y++){
+num = ar3[y]*ar3.length
+console.log (num)}}
+let array3=elemTimesLength(ar3)
+console.log(array3)
